7915bc2a03defc2400f346a191616f916a8a98cf
Jovana Djokovic <jovana.m.djokovic@gmail.com>
2020-09-21 14:08:23 +0200

    REST API for user

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,49 +1,11 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-menager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User',{
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    }, 
-    age: {
-        type: Number,
-        validate(value) {
-            if (value<0) {
-                throw new Error('Age must be a positive number')
-            }
-        },
-        default: 0
-    },
-    email: {
-        type: String,
-        require: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        },
-        trim: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error ('Password can\'t include word password, dummy!')
-            }
-        },
-        trim: true
-    }
-})
+
 
 const Task = mongoose.model('Task', {
     description: {
@@ -57,24 +19,3 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const task = new Task({
-    completed: true
-})
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error ',error)
-})
-
-
-// const me = new User({ 
-//     name: 'Soja',
-//     email: 'soja@gmail.com        ',
-//     password: 'nikolicaprikolica   '
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error ',error)
-// })
