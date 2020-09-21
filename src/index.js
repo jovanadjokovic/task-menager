c808c40de4d9b49ef080e41420471be18e6aab29
Jovana Djokovic <jovana.m.djokovic@gmail.com>
2020-09-21 16:08:42 +0200

    REST API for reading tasks and users

--- src/index.js
+++ src/index.js
@@ -18,6 +18,27 @@ app.post('/users',(req,res) => {
     
 })
 
+app.get('/users',(req,res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) =>{
+        res.status(400).send()
+    })
+})
+
+app.get('/users/:id',(req,res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user)
+            return res.status(404).send()
+        res.send(user)
+        }).catch(() => {
+        res.status(500).send()
+
+    })
+
+})
+
 app.post('/tasks',(req,res) => {
     const task = new Task(req.body)
     task.save().then(() => {
@@ -27,6 +48,28 @@ app.post('/tasks',(req,res) => {
     })
 })
 
+app.get('/tasks',(req,res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(404).send()
+    })
+
+})
+
+app.get('/tasks/:id',(req,res) => {
+    _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ',port)
 })
+
