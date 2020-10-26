902fa38d6005dceae801ad733cd9454f213bef8b
Jovana Djokovic <jovana.m.djokovic@gmail.com>
2020-10-26 16:08:03 +0100

    Sredjena app

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-menager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true 
 })
-
-
-
